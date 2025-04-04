2b6e8a46b8b21db2ddbe0050d1062b8a5dfad542
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-04-04 15:07:11 -0300

    Project created

---
+++ src/main/java/com/testcourse/testcourse/TestcourseApplication.java
@@ -0,0 +1,13 @@
+package com.testcourse.testcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testcourse/testcourse/TestcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testcourse.testcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
