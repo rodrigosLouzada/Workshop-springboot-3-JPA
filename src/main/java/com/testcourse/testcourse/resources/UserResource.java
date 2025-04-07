ad689e3711ab44ff9f67d06d1a02048e71075f0f
Rodrigo Louzada <rodrigolouzada.dev@gmail.com>
2025-04-07 15:25:25 -0300

    resource-service-repository getting users

--- src/main/java/com/testcourse/testcourse/resources/UserResource.java
+++ src/main/java/com/testcourse/testcourse/resources/UserResource.java
@@ -1,19 +1,27 @@
 package com.testcourse.testcourse.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.testcourse.testcourse.entities.User;
+import com.testcourse.testcourse.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "bolsonaro", "bolsonaro@gmail", "999999999", "2026");
+	public ResponseEntity<List<User>> findAll(){
+		
+		List<User> u = service.findAll(); 
 		return ResponseEntity.ok().body(u);
 	}
 }
